a24875ce499acbd17541b6cccaa3319ceea1fd68
JuzChoco <2100812@sit.singaporetech.edu.sg>
2023-09-14 03:18:21 +0800

    Add project files.

---
+++ Engine/Location.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Location
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+
+        public Location(int id, String name, String description)
+        {
+            ID = id;
+            Name = name;
+            Description = description;
+        }
+    }
+}
---
+++ Engine/Quest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Quest
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public int RewardExpereincePoints { get; set; }
+        public int RewardGold { get; set; }
+
+        public Quest(int id, String name, String description, int rewardExperiencePoints, int rewardGold)
+        {
+            ID = id;
+            Name = name;
+            Description = description;
+            RewardExpereincePoints = rewardExperiencePoints;
+            RewardGold = rewardGold;
+        }
+    }
+}
